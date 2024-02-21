c55f500f2f0c808b0a2bcf8a2b4405c804ac07e5
josephnyjs <josephny12@gmail.com>
2024-02-21 00:38:18 -0500

    cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    //configure the size of the windows inside cypress while testing
+      "viewportwidth": 1500,
+      "viewportHeight": 900,
+
+      //HTTP security
+      "chromeWebSecurity": false,
   },
 });
